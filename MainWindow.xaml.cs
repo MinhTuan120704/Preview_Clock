1412cc1e383b84561195889187ef7684289f2dd6
MinhTuan120704 <minhtuan120704@gmail.com>
2023-12-10 12:46:57 +0700

    Preview solution

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Clock
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer _timer;
+        private const string _startTimeDisplay = "00:00:00.00";
+        private const string fastest = "Fastest";
+        private const string slowest = "Slowest";
+        private string _Display;
+        private string _flagDisplay;
+        private long check_slowest = long.MinValue;
+        private long check_fastest = long.MaxValue;
+        private int slowest_index = 0;
+        private int fastest_index = 0;
+        private int pre_slowest_index = 0;
+        private int pre_fastest_index = 0;
+        private int h, m, s, ms, laps = -1;
+        private int fh, fm, fs, fms;
+        public MainWindow()
+        {
+            InitializeComponent();
+            TimeDisplay.Text = _startTimeDisplay;
+            h = 0;
+            m = 0;
+            s = 0;
+            ms = 0;
+            fh = 0; fm = 0; fs = 0; fms = 0;
+            btnLaps.IsEnabled = false;
+
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromMilliseconds(10);
+            _timer.Tick += _timer_Tick;
+        }
+
+        private void _timer_Tick(object sender, EventArgs e)
+        {
+            _Display = string.Empty;
+            ms++;
+            if(ms == 100)
+            {
+                ms = 0;
+                s ++;
+            }
+            if(s == 60)
+            {
+                s = 0;
+                m ++;
+            }
+            if(m == 60)
+            {
+                m = 0;
+                h ++;
+            }
+
+            if (h < 10) _Display += "0" + h.ToString() + ":";
+            else _Display += h.ToString() + ":";
+
+            if (m < 10) _Display += "0" + m.ToString() + ":";
+            else _Display += m.ToString() + ":";
+
+            if (s < 10) _Display += "0" + s.ToString() + ".";
+            else _Display += s.ToString() + ".";
+
+            if (ms < 10) _Display += "0" + ms.ToString();
+            else _Display += ms.ToString();
+
+            TimeDisplay.Text = _Display;
+        }
+
+        private long Subtract_Time()
+        {
+            long time1 = ms*10 + s*1000 + m*60*1000 + h*60*60*1000;
+            long time2 = fms * 10 + fs * 1000 + fm * 60 * 1000 + fh * 60 * 60 * 1000;
+            long res = time1 - time2;
+            return res;
+        }
+
+        private void Set_F_S(long res)
+        {
+            if (res > check_slowest)
+            {
+                check_slowest = res;
+                pre_slowest_index = slowest_index;
+                slowest_index = SpSpeed.Children.Count - laps - 1;
+            }
+            if (res < check_fastest)
+            {
+                check_fastest = res;
+                pre_fastest_index = fastest_index;
+                fastest_index = SpSpeed.Children.Count - laps - 1;
+            }
+        }
+
+
+        private void Set_FlagDisplay()
+        {
+            _flagDisplay = string.Empty;
+
+            long res = Subtract_Time();
+            Set_F_S(res);
+
+            int rh = (int)(res / 60 / 60 / 1000); 
+            res -= rh * 60 * 60 * 1000;
+
+            int rm = (int)(res / 60 / 1000);
+            res -= rm * 60 * 1000;
+
+            int rs = (int)(res / 1000);
+            res -= rs * 1000;
+
+            int rms = (int)(res / 10);
+
+            if (rh < 10) _flagDisplay += "0" + rh.ToString() + ":";
+            else _flagDisplay += rh.ToString() + ":";
+
+            if (rm < 10) _flagDisplay += "0" + rm.ToString() + ":";
+            else _flagDisplay += rm.ToString() + ":";
+
+            if (rs < 10) _flagDisplay += "0" + rs.ToString() + ".";
+            else _flagDisplay += rs.ToString() + ".";
+
+            if (rms < 10) _flagDisplay += "0" + rms.ToString();
+            else _flagDisplay += rms.ToString();
+
+            fh = h;
+            fm = m;
+            fs = s;
+            fms = ms;
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            if (_timer.IsEnabled.ToString() == "False")
+            {
+                _timer.Start();
+                btnStart.Content = "Stop";
+                btnLaps.IsEnabled = true;
+            }
+            else
+            {
+                _timer.Stop();
+                btnStart.Content = "Start";
+                btnLaps.IsEnabled = false;
+            }
+        }
+
+        private void btnLaps_Click(object sender, RoutedEventArgs e)
+        {
+            if(SpLaps.Visibility == Visibility.Hidden)
+            {
+                SpLaps.Visibility = Visibility.Visible;
+                SpTime.Visibility = Visibility.Visible;
+                SpTotal.Visibility = Visibility.Visible;
+                SpSpeed.Visibility = Visibility.Visible;
+                txblLaps.Visibility = Visibility.Visible;
+                txblTime.Visibility = Visibility.Visible;
+                txblTotal.Visibility = Visibility.Visible; 
+            }
+            
+            laps++;
+            TextBlock txblSLaps = new TextBlock();
+            TextBlock txblSTime = new TextBlock();
+            TextBlock txblSTotal = new TextBlock();
+            TextBlock txblSSpeed = new TextBlock();
+            
+            txblSLaps.Text = (laps+1).ToString();
+            txblSLaps.FontSize = 20;
+            txblSLaps.VerticalAlignment = VerticalAlignment.Top;
+            txblSLaps.HorizontalAlignment = HorizontalAlignment.Center;
+            txblSLaps.Foreground = new SolidColorBrush(Colors.White);
+            SpLaps.Children.Insert(0,txblSLaps);
+
+            txblSSpeed.Text = string.Empty;
+            txblSSpeed.FontSize = 20;
+            txblSSpeed.VerticalAlignment = VerticalAlignment.Top;
+            txblSSpeed.HorizontalAlignment = HorizontalAlignment.Center;
+            txblSSpeed.Foreground = new SolidColorBrush(Colors.White);
+            SpSpeed.Children.Insert(0, txblSSpeed);
+
+            if(laps >= 1)
+            {
+                pre_fastest_index++;
+                fastest_index++;
+                pre_slowest_index++;
+                slowest_index++;
+            }
+
+            Set_FlagDisplay();
+            txblSTime.Text = _flagDisplay;
+            txblSTime.FontSize = 20;
+            txblSTime.VerticalAlignment = VerticalAlignment.Top;
+            txblSTime.HorizontalAlignment = HorizontalAlignment.Center;
+            txblSTime.Foreground = new SolidColorBrush(Colors.White);
+            SpTime.Children.Insert(0, txblSTime);
+
+            txblSTotal.Text = TimeDisplay.Text;
+            txblSTotal.FontSize = 20;
+            txblSTotal.VerticalAlignment = VerticalAlignment.Top;
+            txblSTotal.HorizontalAlignment = HorizontalAlignment.Center;
+            txblSTotal.Foreground = new SolidColorBrush(Colors.White);
+            SpTotal.Children.Insert(0, txblSTotal);          
+
+            if (laps >= 1)
+            {
+                TextBlock ft = new TextBlock();
+                ft.FontSize = 20;
+                ft.HorizontalAlignment = HorizontalAlignment.Center;
+                ft.VerticalAlignment = VerticalAlignment.Top;
+                ft.Foreground = new SolidColorBrush(Colors.White);
+                ft.Text = fastest;
+
+                TextBlock st = new TextBlock();
+                st.FontSize = 20;
+                st.HorizontalAlignment = HorizontalAlignment.Center;
+                st.VerticalAlignment = VerticalAlignment.Top;
+                st.Foreground = new SolidColorBrush(Colors.White);
+                st.Text = slowest;
+
+                TextBlock f = new TextBlock();
+                f.FontSize = 20;
+                f.HorizontalAlignment = HorizontalAlignment.Center;
+                f.VerticalAlignment = VerticalAlignment.Top;
+                f.Text = string.Empty;
+
+                TextBlock g = new TextBlock();
+                g.Text = string.Empty;
+                g.FontSize = 20;
+                g.HorizontalAlignment = HorizontalAlignment.Center;
+                g.VerticalAlignment = VerticalAlignment.Top;
+
+
+                if (pre_slowest_index == fastest_index)
+                {
+                    SpSpeed.Children.RemoveAt(pre_fastest_index);
+                    SpSpeed.Children.Insert(pre_fastest_index, f);
+                    SpSpeed.Children.RemoveAt(fastest_index);
+                    SpSpeed.Children.Insert(fastest_index, ft);
+
+
+                    SpSpeed.Children.RemoveAt(slowest_index);
+                    SpSpeed.Children.Insert(slowest_index, st);
+                }
+                else
+                {
+                    SpSpeed.Children.RemoveAt(pre_fastest_index);
+                    SpSpeed.Children.Insert(pre_fastest_index, f);
+                    SpSpeed.Children.RemoveAt(fastest_index);
+                    SpSpeed.Children.Insert(fastest_index, ft);
+
+                    SpSpeed.Children.RemoveAt(pre_slowest_index);
+                    SpSpeed.Children.Insert(pre_slowest_index, g);
+                    SpSpeed.Children.RemoveAt(slowest_index);
+                    SpSpeed.Children.Insert(slowest_index, st);
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            _timer.Stop();
+            SpLaps.Visibility = Visibility.Hidden;
+            SpTime.Visibility = Visibility.Hidden;
+            SpTotal.Visibility = Visibility.Hidden; 
+            SpSpeed.Visibility = Visibility.Hidden;
+            txblLaps.Visibility = Visibility.Hidden;
+            txblTime.Visibility = Visibility.Hidden;
+            txblTotal.Visibility = Visibility.Hidden;
+            TimeDisplay.Text = _startTimeDisplay;
+            _flagDisplay = _startTimeDisplay;
+            fh = 0; fm = 0; fs = 0; fms = 0;
+            h = 0; m = 0; s = 0; ms = 0;
+            laps = -1;
+            slowest_index = 0;
+            fastest_index = 0;
+            pre_slowest_index = 0;
+            pre_fastest_index = 0;
+            check_slowest = long.MinValue;
+            check_fastest = long.MaxValue;
+            SpLaps.Children.RemoveRange(0, SpLaps.Children.Count);
+            SpTime.Children.RemoveRange(0, SpTime.Children.Count);
+            SpTotal.Children.RemoveRange(0, SpTotal.Children.Count);
+            SpSpeed.Children.RemoveRange(0, SpSpeed.Children.Count);
+            
+        }
+    }
+}
---
+++ Pomodoro/MainWindow.xaml.cs
@@ -0,0 +1,449 @@
+﻿using Pomodoro.Control;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Pomodoro
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<int> time = new List<int>() {1, 10, 15, 20, 25, 30, 35, 40, 45, 50, 60, 90, 120, 150, 180, 210, 240 };
+        List<int> break_time = new List<int>() { 1, 2, 3, 4, 5, 10, 15, 20, 25, 30 };
+
+        static int count = 3;
+        static int BreakTime_count = 4;
+        private DispatcherTimer _timer;
+        private DispatcherTimer breakTime;
+        private string Display;
+        private bool f_or_b = true;
+        private int  fm, fs, bm, bs;
+        private int  dfm, dfs, dbm, dbs;
+        private bool? have_breaks;
+        private int NumberOfPomodoros;
+        public MainWindow()
+        {
+            InitializeComponent();
+            txblFocusTime.Text = time[0].ToString();
+            lbBreak_time.Content = break_time[4].ToString();
+
+            Control_Window.SetIntials(this);
+            _timer = new DispatcherTimer();
+
+            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer.Tick += _timer_Tick;
+
+            breakTime = new DispatcherTimer();
+            breakTime.Interval = TimeSpan.FromSeconds(1);
+            breakTime.Tick += BreakTime_Tick;
+        }
+
+        private void BreakTime_Tick(object sender, EventArgs e)
+        {
+            Display = string.Empty;
+            if (bs == 0 && bm == 0)
+            {
+                breakTime.Stop();
+                f_or_b = true;
+                bm = dbm;
+                bs = dbs;
+
+                if (MessageBox.Show("Break time has left", "Clock", MessageBoxButton.OK) == MessageBoxResult.OK)
+                {
+                    _timer.Start();
+                }
+            }
+            else
+            {
+                if (bs == 0)
+                {
+                    bm -= 1;
+                    bs = 60;
+                }
+
+                bs--;
+
+                if (bm < 10) Display += '0' + bm.ToString() + ':';
+                else Display += bm.ToString() + ':';
+
+                if (bs < 10) Display += '0' + bs.ToString();
+                else Display += bs.ToString();
+
+                txblCountdown.Text = Display;
+            }
+        }
+
+        private void Init()
+        {
+            int focustime = Get_Focus_Time();
+            int breaktime = Get_Break_Time();
+
+            dfm = focustime;
+            fm = focustime;
+            fs = 0;
+            dfs = 0;
+            dbm = breaktime;
+            bm = breaktime;
+            bs = 0;
+            dbs = 0;
+            txblCountdown.Text = '0' + fm.ToString() + ":0" + fs.ToString();
+            have_breaks = Cb_Have_Breaks.IsChecked;
+
+            if(have_breaks == false)
+            {
+                if(Cb_Until_Stop.IsChecked == true) NumberOfPomodoros = 11;
+                else NumberOfPomodoros = Int32.Parse(txbTimes.Text) - 1;
+            }
+        }
+
+        private void _timer_Tick(object sender, EventArgs e)
+        {
+            Display = string.Empty;
+            if(fs == 0 && fm == 0)
+            {
+                _timer.Stop();
+                
+
+                if(have_breaks == true || NumberOfPomodoros == 0 && MessageBox.Show("Focus session has ended", "Clock", MessageBoxButton.OK) == MessageBoxResult.OK)
+                {
+                    Cv_Break.Visibility = Visibility.Visible;
+                    Cv_Focus.Visibility = Visibility.Visible;
+                    Cv_Countdown.Visibility = Visibility.Hidden;
+                    Cb_Have_Breaks.IsChecked = false;
+
+                }
+                else if(MessageBox.Show("Focus time has left", "Clock", MessageBoxButton.OK) == MessageBoxResult.OK)
+                {
+                    f_or_b = false;
+                    fm = dfm;
+                    fs = dfs;
+                    if (NumberOfPomodoros <= 10) NumberOfPomodoros--;
+                    breakTime.Start();                  
+                }
+            }   
+            else
+            {
+                if (fs == 0)
+                {
+                    fm -= 1;
+                    fs = 60;
+                }
+
+                fs--;
+
+                if (fm < 10) Display += '0' + fm.ToString() + ':';
+                else Display += fm.ToString() + ':';
+
+                if (fs < 10) Display += '0' + fs.ToString();
+                else Display += fs.ToString();
+
+                txblCountdown.Text = Display;
+            }
+        }
+
+        void Set_index_textchanged()
+        {
+            long focus_time = Get_Focus_Time();
+            for (int i = 1; i < time.Count - 1; i++)
+            {
+                if (time[i] == focus_time)
+                {
+                    count = i;
+                    break;
+                }
+                if (time[i] > focus_time)
+                {
+                    count = i - 1;
+                    break;
+                }
+            }
+        }
+
+        private int Get_Focus_Time()
+        {
+            int focus_time = int.Parse(txblFocusTime.Text);
+            return focus_time;
+        }
+
+        private int Get_Break_Time()
+        {
+            if (Cb_Have_Breaks.IsChecked == false)
+            {
+                int breaktime = Int32.Parse((string)lbBreak_time.Content);
+                return breaktime;
+            }
+            else return 0;
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Control_Window.Exit();
+        }
+
+        private void Maximize_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            Control_Window.DoMaximize(this, btn);
+        }
+
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            Control_Window.Minimize(this);
+        }
+
+        private void Drag_Move(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void Down_Click(object sender, RoutedEventArgs e)
+        {
+            if (count > 0)
+            {
+                count--;
+                txblFocusTime.Text = time[count].ToString();
+            }
+        }
+
+        private void Up_Click(object sender, RoutedEventArgs e)
+        {
+            if (count < time.Count - 1)
+            {
+                count++;
+                txblFocusTime.Text = time[count].ToString();
+            }
+        }
+
+        private void txblFocusTime_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (txblFocusTime.Text != string.Empty)
+            {
+                if (Get_Focus_Time() < time[0])
+                {
+                    txblFocusTime.Text = time[0].ToString();
+                    count = 0;
+                }
+                else if (Get_Focus_Time() > time[time.Count - 1])
+                {
+                    txblFocusTime.Text = time[time.Count - 1].ToString();
+                    count = time.Count-1;
+                }
+                else
+                {
+                    Set_index_textchanged();
+                }
+            }
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Main_Grid.Focus();
+        }
+
+        private void BreakTime_Up_Click(object sender, RoutedEventArgs e)
+        {
+            if (BreakTime_count < break_time.Count - 1)
+            {
+                BreakTime_count++;
+                lbBreak_time.Content = break_time[BreakTime_count].ToString();
+            }
+        }
+
+        private void BreakTime_Down_Click(object sender, RoutedEventArgs e)
+        {
+            if (BreakTime_count > 0)
+            {
+                BreakTime_count--;
+                lbBreak_time.Content = break_time[BreakTime_count].ToString();
+            }
+        }
+
+        private void Pause_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(f_or_b == true)
+            {
+                if((string)Pause_Button.Content == "Pause")
+                {
+                    _timer.Stop();
+                    Pause_Button.Content = "Continue";
+                    Back_Button.IsEnabled = true;
+                }
+                else
+                {
+                    _timer.Start();
+                    Pause_Button.Content = "Pause";
+                    Back_Button.IsEnabled = false;
+                }
+            }
+            else
+            {
+                if ((string)Pause_Button.Content == "Pause")
+                {
+                    breakTime.Stop();
+                    Pause_Button.Content = "Continue";
+                    Back_Button.IsEnabled = true;
+                }
+                else
+                {
+                    breakTime.Start();
+                    Pause_Button.Content = "Pause";
+                    Back_Button.IsEnabled = false;
+                }
+            }
+        }
+
+        private void Back_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(f_or_b == true && MessageBox.Show("Focus session has ended", "Clock", MessageBoxButton.OK) == MessageBoxResult.OK)
+            {
+                _timer.Stop();
+                Cv_Break.Visibility = Visibility.Visible;
+                Cv_Focus.Visibility = Visibility.Visible;
+                Cv_Countdown.Visibility = Visibility.Hidden;
+                Pause_Button.Content = "Pause";
+                Back_Button.IsEnabled = false;
+            }
+            if(f_or_b == false && MessageBox.Show("Focus session has ended", "Clock", MessageBoxButton.OK) == MessageBoxResult.OK)
+            {
+                breakTime.Stop();
+                Cv_Break.Visibility = Visibility.Visible;
+                Cv_Focus.Visibility = Visibility.Visible;
+                Cv_Countdown.Visibility = Visibility.Hidden;
+                f_or_b = true;
+                Pause_Button.Content = "Pause";
+                Back_Button.IsEnabled = false;
+            }
+        }
+
+        private void Skip_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(f_or_b == true)
+            {
+                _timer.Stop();
+
+
+                if (have_breaks == true && MessageBox.Show("Focus session has ended", "Clock", MessageBoxButton.OK) == MessageBoxResult.OK || NumberOfPomodoros == 0 && MessageBox.Show("Focus session has ended", "Clock", MessageBoxButton.OK) == MessageBoxResult.OK)
+                {
+                    Cv_Break.Visibility = Visibility.Visible;
+                    Cv_Focus.Visibility = Visibility.Visible;
+                    Cv_Countdown.Visibility = Visibility.Hidden;
+                    Pause_Button.Content = "Pause";
+                    Back_Button.IsEnabled = false;
+
+                }
+                else if (MessageBox.Show("Focus time has left", "Clock", MessageBoxButton.OK) == MessageBoxResult.OK)
+                {
+                    f_or_b = false;
+                    fm = dfm;
+                    fs = dfs;
+                    if (NumberOfPomodoros <= 10) NumberOfPomodoros--;
+                    Pause_Button.Content = "Pause";
+                    Back_Button.IsEnabled = false;
+                    breakTime.Start();
+                }
+            }
+            else
+            {
+                breakTime.Stop();
+                f_or_b = true;
+                bm = dbm;
+                bs = dbs;
+
+                if (MessageBox.Show("Break time has left", "Clock", MessageBoxButton.OK) == MessageBoxResult.OK)
+                {
+                    Pause_Button.Content = "Pause";
+                    Back_Button.IsEnabled = false;
+                    _timer.Start();
+                }
+            }
+        }
+
+        private void CheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            if(Cb_Have_Breaks.IsChecked == true) 
+            {
+                BreakTime_Up_Button.IsEnabled = false;
+                BreakTime_Down_Button.IsEnabled = false;
+                Cb_Until_Stop.IsEnabled = false;
+                Times_Down_Button.IsEnabled = false;
+                Times_Up_Button.IsEnabled = false;
+                txbTimes.IsEnabled = false;
+            }
+            else
+            {
+                BreakTime_Up_Button.IsEnabled = true;
+                BreakTime_Down_Button.IsEnabled = true;
+                Cb_Until_Stop.IsEnabled=true;
+                Times_Down_Button.IsEnabled = true;
+                Times_Up_Button.IsEnabled = true;
+                txbTimes.IsEnabled = true;
+            }
+        }
+
+        private void Times_Down_Click(object sender, RoutedEventArgs e)
+        {
+            if(Int32.Parse(txbTimes.Text) > 2) 
+            {
+                txbTimes.Text = (Int32.Parse(txbTimes.Text)-1).ToString();
+            }
+        }
+
+        private void Times_Up_Click(object sender, RoutedEventArgs e)
+        {
+            if (Int32.Parse(txbTimes.Text) < 10)
+            {
+                txbTimes.Text = (Int32.Parse(txbTimes.Text) + 1).ToString();
+            }
+        }
+
+        private void Cb_Until_Stop_Click(object sender, RoutedEventArgs e)
+        {
+            if (Cb_Until_Stop.IsChecked == true)
+            {
+                Times_Down_Button.IsEnabled = false;
+                Times_Up_Button.IsEnabled = false;
+            }
+            else
+            { 
+                Times_Down_Button.IsEnabled = true;
+                Times_Up_Button.IsEnabled = true;
+            }
+        }
+
+        private void txbTimes_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if(Int32.Parse(txbTimes.Text) > 10)
+            {
+                txbTimes.Text = 10.ToString();
+            }
+            if(Int32.Parse(txbTimes.Text) < 1)
+            {
+                txbTimes.Text = 1.ToString();
+            }
+        }
+
+        private void Start_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Cv_Break.Visibility = Visibility.Hidden;
+            Cv_Focus.Visibility = Visibility.Hidden;
+            Cv_Countdown.Visibility = Visibility.Visible;
+            Init();           
+            _timer.Start();
+        }
+    }
+}
